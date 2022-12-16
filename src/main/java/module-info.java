2aea56f3dffaeb8cfc441c4a5facc26145a5ee4c
Lertos <deetasticgaming@gmail.com>
2022-12-16 08:06:38 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.lertos.wallsarebad {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.lertos.wallsarebad to javafx.fxml;
+    exports com.lertos.wallsarebad;
+}
