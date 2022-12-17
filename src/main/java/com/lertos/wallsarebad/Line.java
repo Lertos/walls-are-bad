0e102831942354cfc9a356ff0090aa30b0deeba3
Lertos <deetasticgaming@gmail.com>
2022-12-17 13:01:18 -0400

    Adding the line class logic

--- src/main/java/com/lertos/wallsarebad/Line.java
+++ src/main/java/com/lertos/wallsarebad/Line.java
@@ -1,6 +1,22 @@
 package com.lertos.wallsarebad;
 
+import javafx.scene.canvas.GraphicsContext;
+
 public class Line {
 
+    private final double startX;
+    private final double startY;
+    private final double endX;
+    private final double endY;
 
+    public Line(double startX, double startY, double endX, double endY) {
+        this.startX = startX;
+        this.startY = startY;
+        this.endX = endX;
+        this.endY = endY;
+    }
+
+    public void draw(GraphicsContext gc) {
+        gc.strokeLine(this.startX, this.startY, this.endX, this.endY);
+    }
 }
