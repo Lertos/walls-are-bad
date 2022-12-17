530a341f7b3609640142bc27122abba8aeb9b23c
Lertos <deetasticgaming@gmail.com>
2022-12-17 13:00:59 -0400

    Using the player class to draw the square

--- src/main/java/com/lertos/wallsarebad/Main.java
+++ src/main/java/com/lertos/wallsarebad/Main.java
@@ -14,26 +14,29 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class Main extends Application {
 
+    private double canvasWidth;
+    private double canvasHeight;
     private double currentX = 0;
     private double currentY = 20;
 
     static Player player;
+    static Path path;
 
-    public static void main(String[] args) {
-        player = new Player(10);
-
-        launch();
-    }
+    public static void main(String[] args) { launch(); }
 
     @Override
     public void start(Stage stage) {
         Group root = new Group();
         Canvas canvas = new Canvas(600, 600);
+        canvasWidth = canvas.getWidth();
+        canvasHeight = canvas.getHeight();
+
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
         //Set up the game loop
@@ -41,12 +44,16 @@ public class Main extends Application {
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.playFromStart();
 
+        //Setup initial map and player
+        setupHandlers(stage);
+        player = new Player(canvasWidth / 2, canvasHeight / 2, 10);
+        path = new Path(canvas, 10);
+
+        //Finish setting up the stage and then present it
         root.getChildren().add(canvas);
         stage.setResizable(false);
         stage.setScene(new Scene(root));
         stage.show();
-
-        setupHandlers(stage);
     }
 
     private void setupHandlers(Stage stage) {
@@ -69,8 +76,6 @@ public class Main extends Application {
 
     private void draw(GraphicsContext gc) {
         Canvas canvas = gc.getCanvas();
-        double canvasWidth = canvas.getWidth();
-        double canvasHeight = canvas.getHeight();
 
         //Clear the canvas each frame with a background
         gc.setFill(Color.BLACK);
@@ -79,14 +84,10 @@ public class Main extends Application {
         //Set the properties of the other objects in the canvas
         gc.setFill(Color.WHITE);
         gc.setStroke(Color.GRAY);
-        gc.setLineWidth(5);
+        gc.setLineWidth(path.getLineWidth());
 
-        //TODO: Make this into a class to easily check collision and coordinates
         //The player object - always in the middle
-        double middleX = canvasWidth / 2;
-        double middleY = canvasHeight / 2;
-
-        gc.fillRect(middleX - 10, middleY - 10, 20, 20);
+        player.draw(gc);
 
         //DEBUG: Testing a line
         gc.strokeLine(currentX, currentY, currentX, currentY + 60);
--- src/main/java/com/lertos/wallsarebad/Player.java
+++ src/main/java/com/lertos/wallsarebad/Player.java
@@ -1,5 +1,7 @@
 package com.lertos.wallsarebad;
 
+import javafx.scene.canvas.GraphicsContext;
+
 enum Direction {
     LEFT,
     RIGHT,
@@ -8,10 +10,15 @@ enum Direction {
 
 public class Player {
 
+    private final double startX;
+    private final double startY;
     private Direction direction;
     private int speed;
+    private final int size = 10;
 
-    public Player(int speed) {
+    public Player(double startX, double startY, int speed) {
+        this.startX = startX - (size / 2);
+        this.startY = startY - (size / 2);
         this.direction = Direction.UP;
         this.speed = speed;
     }
@@ -26,5 +33,10 @@ public class Player {
 
     public int getSpeed() {
         return speed;
+    }
+
+
+    public void draw(GraphicsContext gc) {
+        gc.fillRect(startX, startY, size, size);
     }
 }
